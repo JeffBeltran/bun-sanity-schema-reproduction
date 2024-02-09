fc8b11f898703e6054bc6ef49cf7c27a48b8eba3
Jeff Beltran <j.beltran@printfly.com>
2024-02-08 19:17:39 -0800

    init

---
+++ worksWithBoth.cjs
@@ -0,0 +1,31 @@
+const { Schema } = require("@sanity/schema");
+
+const defaultSchema = Schema.compile({
+  name: "myBlog",
+  types: [
+    {
+      type: "object",
+      name: "blogPost",
+      fields: [
+        {
+          title: "Title",
+          type: "string",
+          name: "title",
+        },
+        {
+          title: "Body",
+          name: "body",
+          type: "array",
+          of: [{ type: "block" }],
+        },
+      ],
+    },
+  ],
+});
+
+// The compiled schema type for the content type that holds the block array
+const blockContentType = defaultSchema
+  .get("blogPost")
+  .fields.find((field) => field.name === "body").type;
+
+console.log(blockContentType);
